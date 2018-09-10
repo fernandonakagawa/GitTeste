41e6fdd63ef55d65dbbf6d257811d18067f40cc2
fernandonakagawa <42784984+fernandonakagawa@users.noreply.github.com>
2018-09-10 10:57:07 -0300

    mod1
    mod1

--- src/gitteste/GitTeste.java
+++ src/gitteste/GitTeste.java
@@ -15,7 +15,7 @@ public class GitTeste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        ArrayList<String> lista = new ArrayList<>();
     }
     
 }
